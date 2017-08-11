f8ebdca589b859d870fcdbc8c0330b306a2a8fd1
Benjamin Wasty <benny.wasty@gmail.com>
2017-08-11 22:13:41 +0200

    WIP use new model-index.json for samples

--- gltf-importer/tests/import_examples.rs
+++ gltf-importer/tests/import_examples.rs
@@ -8,8 +8,12 @@
 // except according to those terms.
 
 extern crate gltf_importer;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
 use std::{fs, path};
+use std::collections::HashMap;
 
 use gltf_importer::{Error as ImportError, Importer};
 use std::boxed::Box;
@@ -54,7 +58,43 @@ fn run() -> Result<(), Box<StdError>> {
     Ok(())
 }
 
+// #[test]
+// fn import() {
+//     let _ = run().expect("no errors");
+// }
+
+#[derive(Debug, Deserialize)]
+struct SampleModel {
+    name: String,
+    screenshot: String,
+    // variants: SampleModelVariants,
+    variants: HashMap<String, String>,
+}
+
+// #[derive(Debug, Deserialize)]
+// struct SampleModelVariants {
+//     #[serde(rename = "glTF")]
+//     glTF: String,
+//     #[serde(rename = "glTF-Binary")]
+//     binary: Option<String>,
+//     #[serde(rename = "glTF-Embedded")]
+//     embedded: Option<String>,
+//     #[serde(rename = "glTF-MaterialsCommon")]
+//     materials_common: Option<String>,
+//     #[serde(rename = "glTF-pbrSpecularGlossiness")]
+//     pbr_specular_glossiness: Option<String>,
+//     #[serde(rename = "glTF-techniqueWebGL")]
+//     technique_webgl: Option<String>,
+// }
+
+use std::fs::File;
 #[test]
-fn import() {
-    let _ = run().expect("no errors");
+fn get_sample_models() {
+    let file = File::open("../../../glTF-Sample-Models/2.0/model-index.json").unwrap();
+    let models: Vec<SampleModel> = serde_json::from_reader(file).unwrap();
+    println!("{:#?}", models);
+    for model in &models {
+        println!("https://raw.githubusercontent.com/KhronosGroup/lTF-Sample-Models/master/2.0/{}/glTF/{}",
+            model.name, model.variants["glTF"])
+    }
 }
